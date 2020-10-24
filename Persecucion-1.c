e03002def99937b3e198bc4047caf7ad0fc507b3
Derek Cortez <derekcortez16@gmail.com>
2020-10-23 19:55:43 -0500

    Inicio de programa con struct movil

---
+++ Persecucion-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+struct movil{
+	int coordenadaX_origen, coordenadaY_origen;
+	coordenadaX_origen = 50;
+	coordenadaY_origen = 50;
+};
+
+int main(){
+	
+	int coordenadaX_destino, coordenadaY_destino;
+
+	return 0;
+
+}
